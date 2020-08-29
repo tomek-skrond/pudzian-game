21d599389dd36b4e91aec17823fec7f60d236b75
tomek-skrond <58492207+tomek-skrond@users.noreply.github.com>
2020-08-29 14:03:22 +0200

    added variables for bullet handling

--- Pudzian.h
+++ Pudzian.h
@@ -8,20 +8,23 @@ class Pudzian :
 {
 protected:
 	bool isAttacking;
+	float rotationIndicator;
+	sf::Vector2f direction;
 public:
 	Pudzian();
 	~Pudzian();
 
 	virtual void Move(const float dir_x, const float dir_y, const float &dt);
 	virtual void Update(const float& dt);
-	virtual bool getIsAttacking() { return this->isAttacking; }
-	virtual float getAttackPoints() { return this->Damage; }
+	bool getIsAttacking() { return this->isAttacking; }
+	float getAttackPoints() { return this->Damage; }
+	float getRotationIndicator() { return this->rotationIndicator; }
 	void Render(sf::RenderTarget* target = nullptr);
 	void UpdateRotation(const float indicator);
 	void AttackAnimation(const float indicator);
-	virtual sf::Vector2f getPosition() { return sf::Vector2f(this->sprite.getPosition().x, this->sprite.getPosition().y); }
+	const sf::Vector2f& getPos() { return this->sprite.getPosition(); }
+	const sf::Vector2f& getDir() { return this->direction; }
 	//void EnemyInteraction(EnemyJakob &jakob);
 	//void Attack(EnemyJakob &jakob);
 	void InitPudzian();
 };
-
