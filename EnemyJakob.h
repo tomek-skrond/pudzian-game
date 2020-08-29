be4e1ca9c6d17ea08c6500f253446d9c24a36d45
tomek-skrond <58492207+tomek-skrond@users.noreply.github.com>
2020-08-29 13:46:55 +0200

    new functions
    Update() overload, Follow() function

--- EnemyJakob.h
+++ EnemyJakob.h
@@ -3,20 +3,24 @@
 #include "Entity.h"
 #include "Bullet.h"
 
+class Pudzian;
+
 class EnemyJakob :
 	public Entity
 {
 	
 public:
 	EnemyJakob();
-	EnemyJakob(float Damage, float healthPoints, float posX, float posY);
+	EnemyJakob(float Damage, float healthPoints, float posX, float posY, Pudzian &player,const float dt);
 	virtual sf::Vector2f getPosition() { return sf::Vector2f(this->sprite.getPosition().x, this->sprite.getPosition().y); }
 	virtual float getAttackPoints();
 	virtual void InitJakob();
 	virtual void Update(const float &dt)override;
+	virtual void Update(const float &dt, Pudzian &player);
 	virtual void checkHealthPoints();
 	virtual void Render(sf::RenderTarget* target = nullptr)override;
 	virtual void Attack();
 	virtual void Move(const float dir_x, const float dir_y, const float & dt);
+	virtual void Follow(Pudzian &player,const float dt);
 };
 
